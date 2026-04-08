3d1692831c1eb663765bd9d0805cb76499e0cd9f
Vagner Bessa <vagnerbessa@MacBook-Air-de-Vagner.local>
2026-04-08 10:45:43 -0300

    feat(ui): substitui icone estatico de microfone por SoundWaveIcon animado no DataLab

--- components/DataLabView.tsx
+++ components/DataLabView.tsx
@@ -6,6 +6,7 @@ import { DynamicDashboard, DashboardData, parseDashboard } from './DynamicDashbo
 import { ChatHeaderSphere } from './ChatHeaderSphere';
 import { SmallNadiaSphere } from './SmallNadiaSphere';
 import { useSpeechRecognition } from '../hooks/useSpeechRecognition';
+import SoundWaveIcon from './SoundWaveIcon';
 import DESIGN_SKILL from '../skills/datalab_design.md?raw';
 
 interface DataLabViewProps {
@@ -343,10 +344,7 @@ const DataLabView: React.FC<DataLabViewProps> = ({ onNavigateHome }) => {
                         : 'text-slate-400 hover:text-rose-400 hover:bg-rose-500/10'
                     } disabled:opacity-40`}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                      <path d="M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z" />
-                      <path d="M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z" />
-                    </svg>
+                    <SoundWaveIcon className="w-5 h-5" isListening={isListening} />
                   </button>
                 )}
               </div>
